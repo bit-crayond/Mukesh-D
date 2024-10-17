19c9dff9c9aa3f8e011a5310d76fd6967d8f6280
Nithin-kanagaraj <nithinkanagarajs@gmail.com>
2024-10-17 16:10:06 +0530

    Modified boiler plate

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-
-import Lapview from './navbar/lapview'
 import { Grid } from '@mui/material'
 import Block from './blocks/block'
 
